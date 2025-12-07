8051da0ef4ccbd2da87df6209f0a379e75200dba
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-12-07 10:42:28 +0500

    My Footer Section Completed

--- RentIQ/src/App.jsx
+++ RentIQ/src/App.jsx
@@ -24,26 +24,40 @@ const App = () => {
       <About/>
       <div class='min-w-screen h-80 bg-slate-500 flex p-5'>
 
-       <div class=' flex w-1/5 justify-center items-center gap-4'>
-       <RentIqIcon size={200} className="text-blue-600 cursor-pointer" />
-       <p class='font-serif text-wrap text-1xl font-medium'>My New Tesing Text for the ksnk,ms nkcm,. vmkszxmn ,jdvukqyeg xcv hebkv,mmx zvhwdvj</p>
+       <div class=' flex flex-col w-1/4 justify-center items-center '>
+       <RentIqIcon size={90} className="text-blue-600 cursor-pointer" />
+       <p class='font-serif text-wrap text-1xl font-medium text-center'>My New Tesing Text for the ksnk,ms nkcm,. vmkszxmn ,jdvukqyeg xcv hebkv,mmx zvhwdvj</p>
        </div>
 
-       <div class=' flex w-1/5 justify-center items-center gap-4'>
+       <div class=' flex  flex-col w-1/4 justify-center items-center gap-4'>
+       <p class="font-serif text-xl font-semibold">Platform</p>
+       <div class="flex flex-col cursor-pointer items-center">
+        <p>Email</p>
+       <p>Facebook</p>
+       <p>Twitter</p>
+       </div>
+       </div>
        
-
+       <div class=' flex  flex-col w-1/4 justify-center items-center gap-4'>
+       <p class="font-serif text-xl font-semibold">Support</p>
+       <div class="flex flex-col cursor-pointer items-center">
+      <p>Help Center</p>
+       <p>Agent</p>
+       <p>Dashboard</p>
        </div>
-       
-       <div class=' flex w-1/5 justify-center items-center gap-4'>
-       <RentIqIcon size={200} className="text-blue-600 cursor-pointer" />
-       <p class='font-serif text-wrap text-1xl font-medium'>My New Tesing Text for the ksnk,ms nkcm,. vmkszxmn ,jdvukqyeg xcv hebkv,mmx zvhwdvj</p>
-       </div>
-
-       <div class=' flex w-1/5 justify-center items-center gap-4'>
-       <RentIqIcon size={200} className="text-blue-600 cursor-pointer" />
-       <p class='font-serif text-wrap text-1xl font-medium'>My New Tesing Text for the ksnk,ms nkcm,. vmkszxmn ,jdvukqyeg xcv hebkv,mmx zvhwdvj</p>
        </div>
 
+       <div class=' flex  flex-col w-1/4 justify-center items-center gap-4'>
+       <p class="font-serif text-xl font-semibold">Legal</p>
+       <div class="flex flex-col cursor-pointer items-center">
+        <p>Privacy Policy</p>
+       <p>Terms of Service</p>
+       <p>Cookie Policy</p>
+       </div>
+       </div>
+      </div>
+         <div class="min-w-screen h-20 bg-slate-500 flex justify-center items-center">
+        <p>©MyrentIq.All rights reserved.</p>
       </div>
     </>
   );
